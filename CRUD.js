eceb10cb29e0ae2a9a7e78c09e8f9df144904cf7
DRojasOlea <101580937+DRojasOlea@users.noreply.github.com>
2022-05-28 01:25:18 -0500

    Validación de funciones editar y borrar

--- CRUD.js
+++ CRUD.js
@@ -1,32 +1,61 @@
+let selectedRow = null
+
 function onFormSubmit(){
     let formData = readFormData ();
+    if(selectedRow == null)
     insertNewRecord(formData);
+    else 
+    updateRecord (formData)
     resetForm ();
 }
-function readFromData(){
-    let formData={};
-    formData['fullname'] = document.getElementById('fullName').value;
-    formData['origen'] = document.getElementById('origin').value;
+
+function readFormData(){
+    let formData = {};
+    formData['fullName'] = document.getElementById('fullName').value;
+    formData['origen'] = document.getElementById('origen').value;
     formData['parentesco'] = document.getElementById('parentesco').value;
-    return formData
+    return formData;
 }
 
-function insertNewRecordData (data){
-    let table = document.getElementById(listaInvitados).getElementsByTagName('tbody')[0];
+function insertNewRecord (data) {
+    let table = document.getElementById('listaInvitados').getElementsByTagName('tbody')[0];
     let newRow = table.insertRow(table.length);
-    cellOne = newRow.insertCell(0);
-    cellOne.innerHTML = data.fullname;
-    cellTwo = newRow.insertCell(1);
-    cellTwo.innerHTML = data.origin;
-    cellThree = newRow.insertCell(2);
-    cellThree.innerHTML = data.parentesco;
-    cellFour = newRow.insertCell(3);
-    cellFour.innerHTML =`<a>Editar</a>
-                        <a>Eliminar</a>`; 
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.origen;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.parentesco;
+    cell3 = newRow.insertCell(3);
+    cell3.innerHTML =   `<a onClick='onEdit(this)'>Editar</a>
+                        <a onClick='onDelete(this)'>Eliminar</a>`; 
 }
 
 function resetForm (){
     document.getElementById('fullName').value = '';
-    document.getElementById('origin').value = '';
+    document.getElementById('origen').value = '';
     document.getElementById('parentesco').value = '';
+    selectedRow = null;
+}
+
+function onEdit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById('fullName').value = selectedRow.cells[0].innerHTML;
+    document.getElementById('origen').value = selectedRow.cells[1].innerHTML;
+    document.getElementById('parentesco').value = selectedRow.cells[2].innerHTML;
+}
+
+function updateRecord (formData) {
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.origen;
+    selectedRow.cells[2].innerHTML = formData.parentesco;
+
+}
+
+function onDelete(td){
+    if (confirm('Seguro que quieres borrar este registro?')) {
+    row = td.parentElement.parentElement;
+    document.getElementById('listaInvitados').deleteRow(row.rowIndex);
+    resetForm ();
+    }
 }
